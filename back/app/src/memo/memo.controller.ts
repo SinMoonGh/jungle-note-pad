e72f30e85de30cb1f1fb07797d00fddd5e5fdd57
SinMoonGh <tity0009@naver.com>
2025-06-17 01:47:53 +0900

    feat: crud 수정

--- back/app/src/memo/memo.controller.ts
+++ back/app/src/memo/memo.controller.ts
@@ -1,28 +1,30 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { MemoService } from './memo.service';
 import { Memo } from './entities/Memo.entity';
+import { CreateMemoDto } from './dto/create-memo.dto';
+import { UpdateMemoDto } from './dto/update-memo.dto';
 
 @Controller('memo')
 export class MemoController {
     constructor(private readonly memoService:MemoService){}
 
     @Get()
-    getAll():Memo[]{
+    async getAll():Promise<Memo[]>{
         return this.memoService.getAll();
     }
 
-    @Get("search")
-    search(@Query('name') searchingMemo:string){
-        return `search for a memo ${searchingMemo}`;
-    }
+    // @Get("search")
+    // search(@Query('name') searchingMemo:string){
+    //     return `search for a memo ${searchingMemo}`;
+    // }
 
     @Get(":id")
-    getOne(@Param('id') id:number){
+    async getOne(@Param('id') id:number){
         return this.memoService.getOne(id);
     }
 
     @Post()
-    create(@Body() memoData){
+    create(@Body() memoData:CreateMemoDto){
         return this.memoService.create(memoData);
     }
 
@@ -32,7 +34,7 @@ export class MemoController {
     }
 
     @Patch(':id')
-    patch(@Param('id') id:number, @Body() upData){
+    patch(@Param('id') id:number, @Body() upData:UpdateMemoDto){
         return this.memoService.update(id, upData)
     }
 }
