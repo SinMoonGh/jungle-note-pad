0a0baf457d8f1bf2c2cf7505736596fe44449db2
SinMoonGh <tity0009@naver.com>
2025-06-16 19:59:05 +0900

    feat: memo.service CRUD 구현

--- back/app/src/memo/memo.service.ts
+++ back/app/src/memo/memo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Memo } from './entities/Memo.entity';
 
 @Injectable()
 export class MemoService {
@@ -8,7 +9,7 @@ export class MemoService {
         return this.memos;
     }
 
-    getOne(id:number):Meme{
+    getOne(id:number):Memo{
         const memo = this.memos.find(movie => movie.id === +id);
         if(!memo){
             throw new NotFoundException(`Movie ID 존재하지 않음 : ${id}`);
@@ -18,6 +19,22 @@ export class MemoService {
 
     deleteOne(id:number):boolean{
         this.getOne(id);
-        this.memos.filter(memo)
+        this.memos.filter(memo => memo.id !== +id);
+        return true;
+    }
+
+    create(memoData):boolean{
+        this.memos.push({
+            id: this.memos.length + 1,
+            ...memoData,
+        })
+        return true;
+    }
+
+    update(id:number, memoData):boolean{
+        const memo = this.getOne(id);
+        this.deleteOne(id);
+        this.memos.push({...memo, ...memoData});
+        return true;
     }
 }
