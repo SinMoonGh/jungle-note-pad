8d6bbe6528a8ba3b4bf6557ac88a1a8584d734f6
SinMoonGh <tity0009@naver.com>
2025-06-16 20:17:27 +0900

    feat: memo controller구현

--- back/app/src/memo/memo.controller.ts
+++ back/app/src/memo/memo.controller.ts
@@ -1,6 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { MemoService } from './memo.service';
+import { Memo } from './entities/Memo.entity';
 
 @Controller('memo')
 export class MemoController {
-    
+    constructor(private readonly memoService:MemoService){}
+
+    @Get()
+    getAll():Memo[]{
+        return this.memoService.getAll();
+    }
+
+    @Get("search")
+    search(@Query('name') searchingMemo:string){
+        return `search for a memo ${searchingMemo}`;
+    }
+
+    @Get(":id")
+    getOne(@Param('id') id:number){
+        return this.memoService.getOne(id);
+    }
+
+    @Post()
+    create(@Body() memoData){
+        return this.memoService.create(memoData);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number){
+        return this.memoService.deleteOne(id);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') id:number, @Body() upData){
+        return this.memoService.update(id, upData)
+    }
 }
