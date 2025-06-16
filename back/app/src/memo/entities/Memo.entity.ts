c268f5249b82e1dc703146815fba4d66469c2ecb
SinMoonGh <tity0009@naver.com>
2025-06-17 00:43:05 +0900

    feat: memo entity 수정

--- back/app/src/memo/entities/Memo.entity.ts
+++ back/app/src/memo/entities/Memo.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity() 
 export class Memo{
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     title: string;
+
+    @Column()
     description: string;
+
+    @CreateDateColumn({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' }) //hack : 이 코드의 의미를 전혀 모르겠음
     createDate: Date;
 }
